89f17111146bf8aac22dfbeaf8524c7884078e83
itzme <itzme@local.machine>
2026-03-18 16:43:52 +0530

    Added Value segment for storing data of the program like data section in a process...

--- chunk.c
+++ chunk.c
@@ -6,10 +6,15 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    
+    initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+
+    freeValueArray(&chunk->constants);
+
     initChunk(chunk);
 }
 
@@ -21,4 +26,9 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
     }
 
     chunk->code[chunk->count++] = byte; // Appending byte then increment count
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1;
 }
