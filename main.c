ae80c01f6b7722367a693618188c611b22a61762
itzme <itzme@local.machine>
2026-03-19 17:56:49 +0530

    Added OP_NEGATE

--- main.c
+++ main.c
@@ -10,15 +10,17 @@ int main(int argc, const char* argv[]) {
 
     initChunk(&chunk);
 
-    // [OP_CONSTANT, 6.9, OP_RETURN] it is 3byte of program
+    // [OP_CONSTANT, 6.9, OP_NEGATE, OP_RETURN] it is 4byte of program
     // like 
     // PUSH 6.9
+    // NEGATE
     // RET
 
     int constant = addConstant(&chunk, 6.9);
     writeChunk(&chunk, OP_CONSTANT, 34);
     writeChunk(&chunk, constant, 34);
-    writeChunk(&chunk, OP_RETURN, 34);
+    writeChunk(&chunk, OP_NEGATE, 34);
+    writeChunk(&chunk, OP_RETURN, 34); // more like "return -6.9"
     
     disassembleChunk(&chunk, "Testing chunk");
     interpret(&chunk);
